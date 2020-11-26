e362d9a4467b65bdd38d110479119bd915a31c1f
Matt Haffner <mrhaffner13@gmail.com>
2020-11-25 23:29:40 -0600

    added list_post and display_post to postController

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,11 +1,25 @@
 import Post from '../models/post'
 
 const list_post = (req, res, next) => {
-
+    Post.find({})
+        .exec((err, post_list) => {
+            if (err) return next(err)
+            res.json(post_list);
+    })
 };
 
 const display_post = (req, res, next) => {
-    
+    Post.findById(req.params.id)
+    .exec((err, post) => {
+        if (err) {
+            return next(err)
+        } else if (post == null) {
+            let err = new Error('No Such Post');
+            err.status = 404;
+            return next(err)
+        }
+        res.json(post);
+    })
 };
 
 const create_post = (req, res, next) => {
--- routes/comment.js
+++ routes/comment.js
@@ -2,10 +2,13 @@ import { Router } from 'express';
 const router = Router();
 import * as commentController from '../controllers/commentController'
 
+
+//maybe want to get a list of comment for a post and/or single post
+
 //create a comment
 router.post('/comment', commentController.create_comment)
 
 //delete a comment
-router.delete('/:commentID', commentController.delete_comment)
+router.delete('/:id', commentController.delete_comment)
 
 export default router;
