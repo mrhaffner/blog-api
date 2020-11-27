a3c87824e0ab013d4cf5c3f30ce75b1054d5108d
Matt Haffner <mrhaffner13@gmail.com>
2020-11-26 20:23:37 -0600

    finished all the methods in postController, now to test them

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,5 @@
 import Post from '../models/post'
+import Comment from '../models/comment'
 
 const list_post = (req, res, next) => {
     Post.find({})
@@ -18,21 +19,43 @@ const display_post = (req, res, next) => {
             err.status = 404;
             return next(err)
         }
-        res.json(post);
+        res.json(post); //what does this even mean lmao
     })
 };
 
 const create_post = (req, res, next) => {
-    
+    const post = new Post({
+        title: req.body.title,
+        text: req.body.text,
+    }).save((err) => {
+        if (err) { return next(err); }
+        res.sendStatus(200);
+    })
 };
 
 const update_post = (req, res, next) => {
-    
+    const post = new Post({
+        title: req.body.title,
+        text: req.body.text,
+        date: new Date(req.body.date), //maybe just leave it out?
+        _id: req.params.id
+    });
+    Post.findByIdAndUpdate(req.params.id, post, {}, (err) => {
+        if (err) { return next(err); };
+        res.sendStatus(201);
+    })
 };
 
-const delete_post = (req, res, next) => {
-    
-};
+const delete_post = async (req, res, next) => {
+    try {
+        await Comment.deleteMany( {post: req.params.id} );
+        await Post.findByIdAndRemove(req.params.id);
+        res.sendStatus(200);
+    } catch (err) {
+        debug(err);
+        next(createError(500));
+    }
+}
 
 export { 
     list_post, 
